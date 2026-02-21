7dad69217c9864ffd4a5845567a3c21764ac7fdb
aifs82-langston <aifs82@gmail.com>
2026-02-21 00:26:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -174,8 +174,8 @@ with st.expander("Ver Leyenda y Criterios de Interpretación", expanded=False):
      * 🟦 **Cuadrado:** Fin del período (Mes de cierre).
     
     **Criterios de Interpretación:**
-    * Δ > 0, C$_t$ > 0 → Crecimiento por encima de la tendencia
-    * Δ < 0, C$_t$ > 0 → Decrecimiento por encima de la tendencia
-    * Δ < 0, C$_t$ < 0 → Decrecimiento por debajo de la tendencia
-    * Δ > 0, C$_t$ < 0 → Crecimiento por debajo de la tendencia
+    * ΔC$_t$ > 0, C$_t$ > 0 → Crecimiento por encima de la tendencia
+    * ΔC$_t$ < 0, C$_t$ > 0 → Decrecimiento por encima de la tendencia
+    * ΔC$_t$ < 0, C$_t$ < 0 → Decrecimiento por debajo de la tendencia
+    * ΔC$_t$ > 0, C$_t$ < 0 → Crecimiento por debajo de la tendencia
     """)
