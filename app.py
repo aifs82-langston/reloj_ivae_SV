d7213bd56d70b8a6965c574574ef4e63b41ad436
aifs82-langston <aifs82@gmail.com>
2026-04-21 10:16:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -175,8 +175,8 @@ with st.expander("Ver Leyenda y Criterios de Interpretación", expanded=False):
      * 🟦 **Cuadrado:** Fin del período (Mes de cierre).
     
     **Criterios de Interpretación:**
-    * ΔC$_t$ > 0, C$_t$ > 0 → Crecimiento por encima de la tendencia (Primer cuadrante)
-    * ΔC$_t$ < 0, C$_t$ > 0 → Decrecimiento por encima de la tendencia (Segundo cuadrante)
-    * ΔC$_t$ < 0, C$_t$ < 0 → Decrecimiento por debajo de la tendencia (Tercer cuadrante)
-    * ΔC$_t$ > 0, C$_t$ < 0 → Crecimiento por debajo de la tendencia (Cuarto cuadrante)
+    * ΔC$_t$ > 0, C$_t$ > 0 → Crecimiento del ciclo por encima de la tendencia (Primer cuadrante)
+    * ΔC$_t$ < 0, C$_t$ > 0 → Decrecimiento del ciclo por encima de la tendencia (Segundo cuadrante)
+    * ΔC$_t$ < 0, C$_t$ < 0 → Decrecimiento del ciclo por debajo de la tendencia (Tercer cuadrante)
+    * ΔC$_t$ > 0, C$_t$ < 0 → Crecimiento del ciclo por debajo de la tendencia (Cuarto cuadrante)
     """)
